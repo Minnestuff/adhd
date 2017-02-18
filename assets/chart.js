e46b1e539a9228c1b5f928bd4ca697028b27489c
Amit Nandan P <amit_nndn@yahoo.co.in>
2017-02-18 16:28:19 -0600

    Adding plugin changes

--- assets/chart.js
+++ assets/chart.js
@@ -1,4 +1,16 @@
 $(function() {
-            // Easy pie charts
-            $('.chart').easyPieChart({animate: 1000});
-        });
+	$('.chart').easyPieChart({animate: 1000});
+	$('.button').on('click',function(e){
+		e.preventDefault();
+		var website = $('.website').val();
+		console.log(getLocalStorage());
+	});
+	function storeLocally(data){
+		var allData = getLocalStorage();
+		allData.push(data);
+		localStorage.setItem("sites",JSON.stringify(allData));
+	}
+	function getLocalStorage(){
+		return JSON.parse(localStorage.getItem("sites"));
+	}
+});
